4dedcc555c1e51ca1c147f07c19bc808115ae220
AbhayKatoch <abhaykatoch210@gmail.com>
2025-09-29 23:26:52 +0530

    chore(frontend): add react-redux and @reduxjs/toolkit

--- backend/interview_backend/interview_app/urls.py
+++ backend/interview_backend/interview_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UploadResumeAPIView, StartInterviewAPIView, SubmitAnswerAPIView, CandidatesListAPIView, CandidateDetailAPIView
+from .views import UploadResumeView, StartInterviewView, SubmitAnswerView, CandidatesListAPIView, CandidateDetailAPIView
 
 urlpatterns = [
-    path('upload_resume/', UploadResumeAPIView.as_view(), name='upload_resume'),
-    path('start_interview/', StartInterviewAPIView.as_view(), name='start_interview'),
-    path('submit_answer/', SubmitAnswerAPIView.as_view(), name='submit_answer'),
+    path('upload_resume/', UploadResumeView.as_view(), name='upload_resume'),
+    path('start_interview/', StartInterviewView.as_view(), name='start_interview'),
+    path('submit_answer/', SubmitAnswerView.as_view(), name='submit_answer'),
     path('candidates/', CandidatesListAPIView.as_view(), name='candidates_list'),
     path('candidates/<int:candidate_id>/', CandidateDetailAPIView.as_view(), name='candidate_detail'),
 ]
